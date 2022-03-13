268931fb30cae20b49d2efb010a32af16d13d849
Den <bikerden69@tut.by>
2022-03-14 00:20:00 +0300

    add: 3 stage

--- test_param.js
+++ test_param.js
@@ -1,12 +1,10 @@
-// const packLoadash = require('lodash')
-// var tmpl = packLoadash.chunk(['a', 'b', 'c', 'd'], 3);
+const packLoadash = require('lodash')
+var tmpl = packLoadash.chunk(['a', 'b', 'c', 'd'], 3);
 // console.log("🚀 ~ file: test_param.js ~ line 3 ~ tmpl", tmpl)
 
+var argv = require('minimist')(process.argv.slice(2));
+ console.log(`The name is ${argv._[0]} and the age is ${argv._[1]}`);
 
-// console.log( tmpl ); // <span class="title">Заголовок</span>
-//var chalk2 = require('chalk');
-// var argv = require('minimist')(process.argv.slice(2));
-// console.log(chalk2.red(argv.name));
 
-import { yellow } from 'chalk'
-console.log(yellow('hi!'))
+
+
