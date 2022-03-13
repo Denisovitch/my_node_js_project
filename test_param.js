40804679d2350eb310a963477524877556ae5b72
Denisovitch <denisglazin@gmail.com>
2022-03-13 22:28:25 +0300

    init: node.js task n1

---
+++ test_param.js
@@ -0,0 +1,12 @@
+// const packLoadash = require('lodash')
+// var tmpl = packLoadash.chunk(['a', 'b', 'c', 'd'], 3);
+// console.log("🚀 ~ file: test_param.js ~ line 3 ~ tmpl", tmpl)
+
+
+// console.log( tmpl ); // <span class="title">Заголовок</span>
+//var chalk2 = require('chalk');
+// var argv = require('minimist')(process.argv.slice(2));
+// console.log(chalk2.red(argv.name));
+
+import { yellow } from 'chalk'
+console.log(yellow('hi!'))
